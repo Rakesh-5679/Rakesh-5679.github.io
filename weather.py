c95116a4a7cf719e4557daa806c4c58e9503640b
Rakesh-5679 <149990653+Rakesh-5679@users.noreply.github.com>
2024-07-05 09:32:12 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 dates=["jun10","jun11","jun12","jun13","jun14","jun15","jun16","jun17","jun18","jun19"]
 temp=[30,38,28,35,35,35,35,23,38,27]
 wind=[28,35,29,23,29,28,21,22,18,21]
-ypos=np.arange(len(dates))
+yp=np.arange(len(dates))
 plt.bar(yp-0.2,temp,color='r',width=.4,label="Temp(in Celsius)")
 plt.bar(yp+0.2,wind,color='b',width=.4,label="Wind(in km/hr)")
 plt.xticks(yp,dates)
